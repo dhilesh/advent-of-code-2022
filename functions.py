9e86209429077c0b5f6aaf9419318925e12c58b2
Dhilesh Vasan <dhilesh.vasan@asx.com.au>
2022-12-03 01:22:59 +1100

    Day 2 complete

--- functions.py
+++ functions.py
@@ -18,3 +18,18 @@ def total_calories_per_elf(filename):
 
 def top_n_elf_calories(elf_sums, n):
     return sorted(elf_sums, reverse=True)[:n]
+
+def rock_paper_scissors_score(file, question_part):
+    choice_score = {"X":1,"Y":2,"Z":3}
+    outcome_dict = {"A X":3, "A Y":6, "A Z":0, "B X":0, "B Y":3, "B Z":6, "C X":6, "C Y":0, "C Z":3}
+    decision_dict = {"A":{"X":"Z", "Y":"X", "Z":"Y"}, "B":{"X":"X", "Y":"Y", "Z":"Z"}, "C":{"X":"Y", "Y":"Z", "Z":"X"},}
+    score = 0
+    for line in read_input(file):
+        if question_part == 1:
+            my_choice = line[2]
+            score += choice_score[my_choice] + outcome_dict[line.strip()]
+        else:
+            opp_choice, result = line[0], line[2]
+            my_decision = decision_dict[opp_choice][result]
+            score += choice_score[my_decision] + outcome_dict[ f'{opp_choice} {my_decision}' ]
+    return score
